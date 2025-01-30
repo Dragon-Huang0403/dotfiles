1900d7ef78f8d265e067595cba2ce4659f9a9e0f
Dragon Huang <j0918023423@gmail.com>
2025-01-30 12:21:22 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,49 @@
-const simpleImportSort = require('eslint-plugin-simple-import-sort');
-// const babelParser = require('@babel/eslint-parser');
+const reactRecommended = require('eslint-plugin-react/configs/recommended');
 
-// npm i -g eslint eslint-plugin-simple-import-sort @babel/eslint-parser
+// npm i -g eslint eslint-plugin-simple-import-sort @babel/eslint-parser eslint-plugin-react @babel/core @babel/eslint-parser @babel/preset-react eslint-plugin-import
 
-// eslint.config.js
-module.exports = {
-  // extends: ['airbnb'],
-  plugins: { 'simple-import-sort': simpleImportSort },
-  // languageOptions: {
-  //   parser: babelParser,
-  //   parserOptions: {
-  //     requireConfigFile: false, // So @babel/eslint-parser doesn't need a .babelrc
-  //     ecmaVersion: 'latest',
-  //     sourceType: 'module',
-  //     ecmaFeatures: {
-  //       jsx: true,
-  //     },
-  //     babelOptions: {
-  //       presets: ['@babel/preset-react'],
-  //     },
-  //   },
-  // },
-  rules: {
-    'no-unused-vars': 'error',
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          // 1. Side effect imports at the start. For me this is important because I want to import reset.css and global styles at the top of my main file.
-          ['^\\u0000'],
-          // 2. `react` and packages: Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-          ['^react$', '^@?\\w'],
-          // 3. Absolute imports and other imports such as Vue-style `@/foo`.
-          // Anything not matched in another group. (also relative imports starting with "../")
-          ['^@', '^'],
-          // 4. relative imports from same folder "./" (I like to have them grouped together)
-          ['^\\./'],
-          // 5. style module imports always come last, this helps to avoid CSS order issues
-          ['^.+\\.(module.css|module.scss)$'],
-          // 6. media imports
-          ['^.+\\.(gif|png|svg|jpg)$'],
-        ],
+module.exports = [
+  reactRecommended,
+  {
+    plugins: {
+      'simple-import-sort': require('eslint-plugin-simple-import-sort'),
+      react: require('eslint-plugin-react'),
+      import: require('eslint-plugin-import'),
+    },
+    languageOptions: {
+      parser: require('@babel/eslint-parser'),
+      parserOptions: {
+        requireConfigFile: false, // So @babel/eslint-parser doesn't need a .babelrc
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        babelOptions: {
+          babelrc: false,
+          configFile: false,
+          presets: ['@babel/preset-react'],
+        },
       },
-    ],
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'error',
+      'react/prop-types': 'off',
+
+      'no-unused-vars': 'error',
+
+      // https://github.com/lydell/eslint-plugin-simple-import-sort?tab=readme-ov-file#example-configuration
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-};
+];

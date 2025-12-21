ec65f9440f4c2679ebca7b8d60e914ec485e9f6b
Xuan Long Huang <j0918023423@gmail.com>
2025-12-21 09:47:36 +0100

    removev lvim cnofig

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Change directory to the first argument if it's a directory
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local arg0 = vim.fn.argv(0)
+    if arg0 == "" then return end
+
+    local p = vim.fn.fnamemodify(arg0, ":p")
+    if vim.fn.isdirectory(p) == 1 then
+      vim.cmd("cd " .. vim.fn.fnameescape(p))
+    end
+  end,
+})
